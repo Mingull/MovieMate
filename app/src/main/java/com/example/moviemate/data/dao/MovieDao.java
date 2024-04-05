2d30d61a7c473c8705f968f5a9cd6d6580101692
Mingull <nielsplug@outlook.com>
2024-04-05 03:26:24 +0200

    added onConflict ignore

--- app/src/main/java/com/example/moviemate/data/dao/MovieDao.java
+++ app/src/main/java/com/example/moviemate/data/dao/MovieDao.java
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Update;
 
@@ -15,7 +16,7 @@ import java.util.List;
 public interface MovieDao {
 
     // Insert a movie into the database
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     void insertMovie(Movie movie);
 
     // Delete a movie from the database
